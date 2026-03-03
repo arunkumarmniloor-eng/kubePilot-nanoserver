38493663abb0a50f1d90b772d3cb192b275b8fb3
Ubuntu <ubuntu@ip-172-31-34-23.ap-south-1.compute.internal>
2026-03-03 07:04:10 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+	  if (req.url === "/health") {
+		      res.writeHead(200);
+		      res.end("OK");
+		    } else {
+			        res.writeHead(200);
+			        res.end("Hello from DevOps Project 1 🚀");
+			      }
+});
+
+server.listen(3000, () => {
+	  console.log("Server running on port 3000");
+})
+
